58c1e2004461500221741c7ecd8b7df31fc5956a
Abdirayimov <orifjon12032000@gmail.com>
2021-10-22 19:36:09 +0500

    changed settings

--- djangocustapp/urls.py
+++ djangocustapp/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_URL) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
